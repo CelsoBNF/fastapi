f56e411d30cea818af10d4a5ed6c044c6c34f45c
Celso Natal <85707552+CelsoBNF@users.noreply.github.com>
2022-06-15 13:26:37 -0300

    Update database.py

--- card/database.py
+++ card/database.py
@@ -2,6 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+Deploy in this link: https://cryptic-castle-13164.herokuapp.com/docs#/
+    
+    ## Deploy foi feito usando Heroku
+
 SQLACLHEMY_DATABASE_URL = 'sqlite:///./card.db'
 
 engine = create_engine(SQLACLHEMY_DATABASE_URL, connect_args={
